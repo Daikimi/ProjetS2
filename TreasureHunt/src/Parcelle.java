f1c6f1b613332fc0cfca78bd6ebf6918e28af1c3
Shadovokun <provo.florian@gmail.com>
2016-03-07 13:11:56 +0100

    Added files via upload
    Ajout de la parcelle (essai ?)

---
+++ TreasureHunt/src/Parcelle.java
@@ -0,0 +1,16 @@
+public class Parcelle {
+
+	String case = "";
+	String chose = "";
+	
+
+	public void changement {
+
+			if (case != "") {
+			case = case;
+		} else {
+		case = chose;
+		} 
+	}
+
+}
