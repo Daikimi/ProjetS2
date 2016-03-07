ef8b9dc908c2b1f28c3cca1d8beaf11b9fb07944
Shadovokun <provo.florian@gmail.com>
2016-03-07 18:25:06 +0100

    Ajout formule nbParcelles

--- TreasureHunt/src/Island.java
+++ TreasureHunt/src/Island.java
@@ -2,9 +2,9 @@
 public class Island {
 	
 	
-	public int xIle = 10;
-	public int yIle = 10;
-	public int nbParcelles = 62 ; //Faut voir pour une formule qui excluerait le carrÃ© extÃ©rieur
+	public int xIle = 10; // A modifier si l'on veut
+	public int yIle = 10; // A modifier si l'on veut
+	public int nbParcelles = (xIle*yIle) - ((xIle + yIle)*2) ; // Aire - Périmètre = Intérieur du carré (l'extérieur sera de l'eau)
 	public int nbRochers = 5; //Je sais pas combien on doit en mettre ?
 	public Parcelle[][] ile = new Parcelle[xIle][yIle];
 	
