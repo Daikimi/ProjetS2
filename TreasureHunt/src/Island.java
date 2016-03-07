cd239b49758b3c0b15071080b79cbee7c1de7f0a
Shadovokun <provo.florian@gmail.com>
2016-03-07 21:09:31 +0100

    BLOQUÉ TWT : le retour

--- TreasureHunt/src/Island.java
+++ TreasureHunt/src/Island.java
@@ -5,7 +5,7 @@ public class Island {
 	public int xIle = 10; // A modifier si l'on veut
 	public int yIle = 10; // A modifier si l'on veut
 	public int nbParcelles = (xIle*yIle) - ((xIle + yIle)*2) ; // Aire - Périmètre = Intérieur du carré (l'extérieur sera de l'eau)
-	public int nbRochers = 5; //A modifier, je pense que ce sera trop peu
+	public int nbRochers = (xIle*yIle)/10; // 10% de l'île
 	public Parcelle[][] ile = new Parcelle[xIle][yIle];
 	
 	public String toString() {
