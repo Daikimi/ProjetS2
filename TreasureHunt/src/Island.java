12de00a0ff020a7792bddac8eb9dada67b9818ad
Shadovokun <provo.florian@gmail.com>
2016-03-07 13:23:26 +0100

    Petit démarrage pour la Island..?

--- TreasureHunt/src/Island.java
+++ TreasureHunt/src/Island.java
@@ -1,4 +1,13 @@
 
 public class Island {
 	
+	
+	public int xIle = 10:
+	public int yIle = 10;
+	public int nbParcelles = 62 ; //Faut voir pour une formule qui excluerait le carré extérieur
+	public int nbRochers = 5; //Je sais pas combien on doit en mettre ?
+	public Parcelle[][] ile = new Parcelle[xIle][yIle];
+	
+	
+	
 }
