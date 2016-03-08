b305fc0c00656b74fee68bd07af27878acc43535
Flyxe <antoine.hourdeau1@gmail.com>
2016-03-08 16:40:41 +0100

    08/03 16:40 placement rocher

--- TreasureHunt/src/Island.java
+++ TreasureHunt/src/Island.java
@@ -2,20 +2,30 @@ public class Island {
 	
 	public int xIle = 10; // A modifier si l'on veut
 	public int yIle = 10; // A modifier si l'on veut
-	public int nbParcelles = (xIle*yIle) - ((xIle + yIle)*2) ; // Aire - Périmètre = Intérieur du carré (l'extérieur sera de l'eau)
-	public int nbRochers = (xIle*yIle)/10; //A modifier, je pense que ce sera trop peu
+	public int nbParcelles = (xIle*yIle) - ((xIle + yIle)*2) ; // Aire - Perimetre = Interieur du carre (l'exterieur sera de l'eau)
+	public int nbRochers = (int)((xIle*yIle)*0.1); //A modifier, je pense que ce sera trop peu
 	public Parcelle[][] ile = new Parcelle[xIle][yIle];
 	
 	public Island(){
 		for(int i=0;i<ile.length;i++){
 			for(int j=0;j<ile[0].length;j++){
-				if (i == 0 || i == xIle-1 || j == 0 || j == yIle-1) {
-					ile[i][j]=new Parcelle("X");
-				} else {
-					ile[i][j]=new Parcelle(" ");
-				}
+				ile[i][j]=new Parcelle();
 			}
 		}
+		Random rdm = new random();
+		for(int i =0;i<nbRochers;i++){
+			do{
+				boolean positionne=false;
+				int posX = rdm.nextInt(xIle);
+				int posY = rdm.nextInt(yIle);
+				if(ile[posX][posY]==" "){
+					ile[posX][posY]="R";
+					positionne= true;
+				}
+			}while(!positionne);
+			
+		}
+		
 	}
 		
 	public String toString() {
@@ -31,12 +41,14 @@ public class Island {
 	}
 	
 	public void changement(String chose,int posX,int posY) {	
-		ile[posX][posY].changement(chose); // Je n'arrive pas à modifier de manière à ce que les changements montent... :/
+		ile[posX][posY].changement(chose); // Je n'arrive pas a modifier de manière a ce que les changements montent... :/
 	}
 	
 	public String affichage(int posX,int posY) {
 		return ile[posX][posY].toString(); 
 	}
+	 
+	public void placementRocheux()
 	
 	public static void main(String[] args){
 		
